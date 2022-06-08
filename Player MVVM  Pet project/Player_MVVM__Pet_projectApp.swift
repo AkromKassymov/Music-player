7d0db1a4d29e640151d8f1810de8262875d9b355
Akrom Kassymov <akrom222@gmail.com>
2022-06-08 15:30:58 +0600

    Initial Commit

---
+++ Player MVVM  Pet project/Player_MVVM__Pet_projectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Player_MVVM__Pet_projectApp.swift
+//  Player MVVM  Pet project
+//
+//  Created by Akrom on 08.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct Player_MVVM__Pet_projectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
